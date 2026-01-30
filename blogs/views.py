d092b0793a7c3b19484ce56560ee93e00eb4592d
Anish <singhkrishn704@gmail.com>
2026-01-30 14:44:14 +0530

    Posts by Category

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
 
+from .models import Blog, Category
 # Create your views here.
+def post_by_category(request, category_id):
+    posts = Blog.objects.filter(status='Published', category_id=category_id)
+    try:
+        category= Category.objects.get(pk=category_id)
+    except:
+        #when category is more than the given category is will go to home page
+        return redirect('home')
+    context = {
+        'posts':posts,
+        'category':category,
+    }
+    return render(request,'posts_by_category.html',context)
--- newCart/views.py
+++ newCart/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blogs.models import Category, Blog
 def home(request):
-    return render(request, 'home.html')
+    # categories = Category.objects.all()
+    featured_post = Blog.objects.filter(is_featured=True).order_by('updated_at')
+    posts = Blog.objects.filter(is_featured=False,status = 'Published')
+
+    context = {
+       # 'categories': categories,
+        'featured_post': featured_post,
+        'post':posts,
+    }
+    return render(request, 'home.html', context)
